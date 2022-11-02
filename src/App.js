2f1f733561f85b1862fecaf2c82e578fbf68dd4d
dc <00danzy@gmail.com>
2022-11-02 10:24:54 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 // import logo from './logo.svg';
 import Navbar from './components/Navbar'
 import Home from './components/Home';
-
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+import Create from './components/Create';
 
 // console.log(process.env)
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className='content'>
-        <Home />
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+        <div className='content'>
+          <Routes>
+            <Route path='/' element={ <Home /> } />
+            <Route path='/create' element={ <Create /> } />
+          </Routes>
+        </div>
       </div>
-    </div>
+
+    </BrowserRouter>
   );
 }
 
