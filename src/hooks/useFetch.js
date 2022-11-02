bc1b035bc793387cf529b49b35d6a739b369e252
dc <00danzy@gmail.com>
2022-11-02 10:24:28 +0100

    update

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -4,25 +4,26 @@ const useFetch = (url) => {
    const [blogs, setBlogs] = useState(null)
    
    useEffect(()=> {
-      fetch(url, {
-         method: "GET",
-         headers: {
-            "Authorization": `${process.env.REACT_APP_API_KEY}`
-         }
-      })
-      .then(res => {
-         if (!res.ok) {
-            throw Error('Could not fetch the data for that resource');
-         }
-         return res.json()
-      })
-      .then(data => {
-         console.log(data)
-         setBlogs(data.articles)
-      })
-      .catch(err => {
-         console.log(err.message)
-      })
+      setTimeout(
+         fetch(url, {
+            method: "GET",
+            headers: {
+               "Authorization": `${process.env.REACT_APP_API_KEY}`
+            }
+         })
+         .then(res => {
+            if (!res.ok) {
+               throw Error('Could not fetch the data for that resource');
+            }
+            return res.json()
+         })
+         .then(data => {
+            console.log(data)
+            setBlogs(data.articles)
+         })
+         .catch(err => {
+            console.log(err.message)
+         }), 1000)
    }, [])
 
    return {blogs, setBlogs}
